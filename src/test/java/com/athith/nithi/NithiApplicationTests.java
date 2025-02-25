653b213e992146f51dfd2416698835494a38745e
NimalAthith <tnimalathith@gmail.com>
2025-02-25 08:11:49 +0530

    first

---
+++ src/test/java/com/athith/nithi/NithiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athith.nithi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NithiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
